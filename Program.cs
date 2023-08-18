1ccfc18fbc66d3c340c30e24c1e5a714618b7541
gabriel marcello <gabriel.marcellocf@gmail.com>
2023-08-18 12:44:07 -0300

    Controlando as pecas capturadas

--- Program.cs
+++ Program.cs
@@ -16,10 +16,7 @@ internal class Program
                 try
                 {
                     Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab);
-                    Console.WriteLine();
-                    Console.WriteLine("Turno: " + partida.turno);
-                    Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                    Tela.imprimirPartida(partida);
 
                     Console.WriteLine();
                     Console.Write("Origem: ");
